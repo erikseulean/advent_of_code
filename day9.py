603a303a25780ba8b3c9a387f4a97616622e9bd0
Erik-Cristian Seulean <erikseulean@gmail.com>
2021-12-09 23:34:51 +0000

    Get rid of extra matrix
    Signed-off-by: Erik-Cristian Seulean <erikseulean@gmail.com>

--- day9.py
+++ day9.py
@@ -1,15 +1,10 @@
 matrix = []
-visited = []
-
 
 def basin(i, j):
-    if matrix[i][j] == 9:
+    if matrix[i][j] >= 9:
         return 0
 
-    if visited[i][j]:
-        return 0
-
-    visited[i][j] = True
+    matrix[i][j] = 9
 
     return 1 + basin(i - 1, j) + basin(i + 1, j) + basin(i, j - 1) + basin(i, j + 1)
 
@@ -19,19 +14,17 @@ def add_row(new_row):
     for nr in new_row:
         r.append(int(nr))
     r.append(9)
-    visited.append([0] * (len(new_row) + 2))
     matrix.append(r)
 
 
 with open("d9.txt") as f:
     first_line = f.readline().rstrip()
-    matrix.append([9] * (len(first_line) + 2))
-    visited.append([False] * (len(first_line) + 2))
+    padding = [9] * (len(first_line) + 2)
+    matrix.append(padding)
     add_row(first_line)
     for line in f:
         add_row(line.rstrip())
-    matrix.append([9] * (len(first_line) + 2))
-    visited.append([False] * (len(first_line) + 2))
+    matrix.append(padding)
 
 smallest_sum = 0
 sums = []
